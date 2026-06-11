84fd56e0728a81091cb4f4653b9faf71887a903e
Min Hong Yun <mhyun@google.com>
2026-06-10 19:02:44 -0700

    print some

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ echo "============================"
 echo "Build Seriously..."
 sleep 10
 echo "SUCCESS"
+echo SHORT_SHA $SHORT_SHA
+echo _OVERWRITE_REF_SHA $_OVERWRITE_REF_SHA
+echo a $a
 echo "============================"
