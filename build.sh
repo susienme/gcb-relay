6e4fe23da15a250067d20d0a7ded02ad55c69584
Min Hong Yun <mhyun@google.com>
2026-06-10 19:52:22 -0700

    print more

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo "Build Seriously..."
 sleep 10
 echo "SUCCESS"
 echo SHORT_SHA $SHORT_SHA
-echo _OVERWRITE_REF_SHA $_OVERWRITE_REF_SHA
+echo OVERWRITE_REF_SHA $OVERWRITE_REF_SHA
 echo ASDF $ASDF
 echo a $a
 echo "============================"
