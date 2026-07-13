5d6f2d8b9b670fd82ea0b493688db1f9e9309858
Min Hong Yun <mhyun@google.com>
2026-07-13 16:25:28 -0700

    print LOUHI_REF_SHA

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ echo "SUCCESS"
 echo SHORT_SHA $SHORT_SHA
 echo OVERWRITE_REF_SHA $OVERWRITE_REF_SHA
 echo ASDF $ASDF
+echo LOUHI_REF_SHA $LOUHI_REF_SHA
 echo a $a
 echo "============================"
