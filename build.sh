54d69b4091eee7fcc4008bdab6189b1eb5aedfac
Min Hong Yun <mhyun@google.com>
2026-07-13 17:31:54 -0700

    print sleep

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ LOUHI_REF_SHA="${LOUHI_REF_SHA:-$(git rev-parse HEAD)}"
 echo "============================"
 echo "Build Seriously..."
 echo "SUCCESS"
+echo SLEEP $SLEEP
 echo SHORT_SHA $SHORT_SHA
 echo OVERWRITE_REF_SHA $OVERWRITE_REF_SHA
 echo ASDF $ASDF
